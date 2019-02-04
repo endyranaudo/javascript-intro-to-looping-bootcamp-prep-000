58a51d151be0a057b04010f1b3c8df760e7d3cde
Endy Ranaudo <endyranaudo@gmail.com>
2019-02-04 10:38:57 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,6 +1,7 @@
 for(let i = 1; i <= 25; i++ ){
   if (i == 1){
-    I am 1 strange loop."
+    console.log("I am 1 strange loop.");
+  } else {
+    console.log("I am ${i} strange loops.")
   }
-  console.log(I am ${i} strange loops.")
 }
